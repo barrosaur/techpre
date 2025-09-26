a155e5677ca67eac85d6ffc5485b009aabb91c22
neglectPanda <mypacc006@gmail.com>
2025-09-26 23:27:07 +0800

    signup 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Greeting from "@/components/Greeting";
 import HeaderSign from "@/components/HeaderSign";
 import './page.css'
 
-// import Signup from "@/components/Signup";
+import Signup from "@/components/Signup";
 
 export default function Home() {
   return (
@@ -11,14 +11,12 @@ export default function Home() {
       <HeaderSign/>
       <div className="child-1">
         <div className="left">
-          <Greeting/>
+          {/* <Greeting/> */}
+          <Signup/>
         </div>
 
         <div className="right">
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
+          
         </div>
       </div>
     </div>
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,13 +1,99 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import '@/styles/Signup.css';
+import Image from 'next/image';
 
 const Signup = () => {
+  const imgSize = 30;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPass, setConfirmPass] = useState("")
+  const [seePass, setSeePass] = useState(false)
+
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+    setConfirmPass("");
+  }
+
   return (
     <div className="signup">
       <div className="head-text">
         <h1>Sign Up with Just —</h1>
         <h1>Just Study</h1>
       </div>
+      <hr />
+
+      <form action="">
+        <div className="wrapper">
+          <label htmlFor="">Email</label>
+          <input 
+            type="email"
+            placeholder='Enter your email' 
+            className='email-grn'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="wrapper">
+          <label htmlFor="">Password</label>
+          <div className="view-pw pw-prpl">
+            <input 
+              type={seePass ? "text" : "password"}
+              placeholder='Enter your password' 
+              className='pw-prpl'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required
+            />
+            <button className='see-pass-btn' onClick={() => setSeePass(!seePass)}>
+              <Image
+                src={seePass ? "/images/eye-closed.svg" : "/images/eye-open.svg"}
+                height={imgSize}
+                width={imgSize}
+                alt={seePass ? "Close password" : 'See Password'}
+                className='see-pass'
+              />
+            </button>
+          </div>
+        </div>
+
+        <div className="wrapper">
+          <label htmlFor="">Confirm Password</label>
+          <div className="view-pw conf-pw-blue">
+            <input 
+              type={seePass ? "text" : "password"}
+              placeholder='Confirm password'
+              className='conf-pw-blue' 
+              value={confirmPass}
+              onChange={e => setConfirmPass(e.target.value)}
+              required
+            />
+            <button className='see-pass-btn' onClick={() => setSeePass(!seePass)}>
+              <Image
+                src={seePass ? "/images/eye-closed.svg" : "/images/eye-open.svg"}
+                height={imgSize}
+                width={imgSize}
+                alt={seePass ? "Close password" : 'See Password'}
+                className='see-pass'
+              />
+            </button>
+          </div>
+        </div>
+
+        {
+          (confirmPass !== password) && (
+            <p className="red-msg">Passwords do not match.</p>
+          )
+        }
+
+        <div className="signup-btn-container">
+          <button onClick={clearInputs}>Sign up</button>
+        </div>
+      </form>
     </div>
   )
 }
